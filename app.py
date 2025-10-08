1ae9054f1a71cc661dfe86cf7bc631bc6fd2c330
Ubuntu <ubuntu@ip-172-31-16-109.ec2.internal>
2025-10-08 06:29:13 +0000

    Updated code directly from EC2

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get('/', response_class=HTMLResponse)
 def home():
     return '<h1>Hello Dev Team </h1>'
+
+
+@app.get('/whowrotethis')
+def whowrotethis():
+    return "ahmad alkurdi"
